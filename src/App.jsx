b93cf5c7ab6411b052fb976e45fb5b03df8b86ad
kohil2211 <144275448+kohil2211@users.noreply.github.com>
2026-01-16 13:08:26 +0530

    Add Header component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Story from "./pages/Story"
+import Header from "./Header"
 
 function App() {
   return (
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/story/:id" element={<Story />} />
